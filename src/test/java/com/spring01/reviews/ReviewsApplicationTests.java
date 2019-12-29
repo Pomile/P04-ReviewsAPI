83505697bcbace78afab8dfd0df759a80929d8e6
Babatunde <soft-sky@live.co.uk>
2019-12-29 11:13:42 +0100

    Review with polyglot persistence starter

---
+++ src/test/java/com/spring01/reviews/ReviewsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring01.reviews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ReviewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
