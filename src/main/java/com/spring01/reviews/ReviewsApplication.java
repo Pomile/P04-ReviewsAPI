91a0c48598681ae230889ea33e3a0fcf0daf92b5
Babatunde <soft-sky@live.co.uk>
2019-12-30 17:51:08 +0100

    feat[setup-mongo]: Implement mongodb setup and migration
    - [x] Add dependencies
    - [x] Configure mongobee bean
    - [x] Configure changelog and changeset for database migration

--- src/main/java/com/spring01/reviews/ReviewsApplication.java
+++ src/main/java/com/spring01/reviews/ReviewsApplication.java
@@ -1,7 +1,14 @@
 package com.spring01.reviews;
 
+import com.github.mongobee.Mongobee;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class ReviewsApplication {
@@ -10,4 +17,12 @@ public class ReviewsApplication {
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
 
+	@Bean
+	public Mongobee mongobee() throws Exception {
+
+		Mongobee runner = new Mongobee("mongodb://localhost:27017/");
+		runner.setDbName("reviewsDb");// host must be set if not set in URI
+		runner.setChangeLogsScanPackage("com.spring01.reviews.changelogs"); // the package to be scanned for changesets
+		return runner;
+	}
 }
